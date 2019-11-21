f94b635887432ae740a440b2cdb941b6eac5e0b6
BlackCyber009 <57897184+BlackCyber091@users.noreply.github.com>
2019-11-21 19:02:47 +0700

    Add files via upload

---
+++ Team.sh
@@ -0,0 +1,39 @@
+clear
+
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+
+
+
+echo " _______________________________________" | lolcat
+echo "|                                       |" | lolcat
+echo "|        Nickname Anggota DrCyber       |" | lolcat
+echo "|_______________________________________|" | lolcat
+echo
+echo
+echo "     [ Kumpulan Nak Remaja Coding ]"
+echo "          [ Diam Mengerikan ]"
+echo "        [ Bergerak  Mematikan ]"
+echo "   [ Remaja Berkarya Bukan Bergaya ]"
+echo "        [ Remaja Anti Narkoba ]"
+echo "   [ Remaja Keren  Tanpa Asap Roko ]"
+echo
+echo "<=========================================>" | lolcat
+echo "[+]RanggaHarsya01 (Leader)"
+echo "[+]Mr.Devil (Co Leader)"
+echo "[+]Ghost_id (Co Founder)"
+echo "[+]ArdiSaputra27"
+echo "[+]Mr.Matrix"
+echo "[+]Mr.id"
+echo "[+]Arlan.Z"
+echo "[+]Mr.unknown"
+echo "[+]Mr.Plasma"
+echo "[+]DrZlf_17"
+echo "[+]Mr.Bitch"
+echo "<=========================================>" | lolcat
+
